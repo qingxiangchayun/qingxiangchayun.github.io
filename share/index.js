66877ddfc7a9604ce4ec67695fd36bef72e4eb97
qingxiangchanyun <yuanli891212@163.com>
2015-05-06 18:32:29 +0800

    add blog

--- share/index.js
+++ share/index.js
@@ -8,4 +8,25 @@ Person.prototype.showName = function(){
 
 var xm = new Person('xiaoming');
 
+function Student (name,grade){
+	Person.call(this,name);
+	this.grade = grade;
+}
+
+
+function extend(target,source,deep){
+    for(var key in source){
+        if(deep && typeof source[key] === 'object'){
+            target[key] = extend({},source[key],deep);
+        }else{
+            target[key] = source[key];
+        }
+    }
+   
+    return target;
+}
+
+var xh = new Student('xiaohong','college');
+
+
 
